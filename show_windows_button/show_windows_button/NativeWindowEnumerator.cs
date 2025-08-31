a6b38a37cce0bbe61e071854732c31afab6006a5
Я <>
2025-08-31 15:14:07 +0300

    -- NativeWindowEnumerator fix

--- show_windows_button/show_windows_button/NativeWindowEnumerator.cs
+++ show_windows_button/show_windows_button/NativeWindowEnumerator.cs
@@ -1,27 +1,23 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Media.Imaging;
+using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Windows.ApplicationModel.Core;
-using Windows.Graphics.Imaging;
-using Windows.Storage.Streams;
+using System.Text;
 
 namespace show_windows_button
 {
     public static class NativeWindowEnumerator
     {
-        #region P/Invoke
+        #region Win32
         private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
         [DllImport("user32.dll", SetLastError = true)]
         private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
         [DllImport("user32.dll")]
         private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
@@ -33,14 +29,12 @@ namespace show_windows_button
         [DllImport("user32.dll")]
         private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
-
         private const int GWL_EXSTYLE = -20;
         private const int WS_EX_TOOLWINDOW = 0x00000080;
         private const int WS_EX_APPWINDOW = 0x00040000;
+        #endregion
 
-        // Virtual-desktop helpers (Win10+)
+        #region Virtual-desktop COM
         [ComImport, Guid("A5CD92FF-29BE-454C-8D04-D82879FB3F1B")]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
         private interface IVirtualDesktopManager
@@ -51,77 +45,112 @@ namespace show_windows_button
         [ComImport, Guid("AA509086-5CA9-4C25-8F95-589D3C07B48A")]
         private class VirtualDesktopManager { }
 
-        private static readonly IVirtualDesktopManager _vdm =
-            (IVirtualDesktopManager)new VirtualDesktopManager();
+        [ComImport]
+        [Guid("6D5140C1-7436-11CE-8034-00AA006009FA")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IServiceProvider
+        {
+            [PreserveSig]
+            int QueryService(
+                ref Guid guidService,
+                ref Guid riid,
+                [MarshalAs(UnmanagedType.IUnknown)] out object ppvObject);
+        }
+
+        [ComImport, Guid("F31574D6-B682-4CDC-BD56-1827860ABEC6")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IVirtualDesktopManagerInternal
+        {
+            int GetCount(IntPtr hWndOrMon);
+            void GetDesktops(out IObjectArray desktops);
+            void GetDesktop(int index, out IVirtualDesktop desktop);
+        }
+
+        [ComImport, Guid("92CA9DCD-5622-4BBA-A805-5E9F541BD8C9")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IObjectArray
+        {
+            void GetCount(out int count);
+            void GetAt(int index, ref Guid riid, [MarshalAs(UnmanagedType.Interface)] out object ppvObject);
+        }
+
+        [ComImport, Guid("FF72FFDD-BE7E-43FC-9C03-AD81681E88E4")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IVirtualDesktop
+        {
+            Guid GetId();
+        }
         #endregion
 
+        private static readonly IVirtualDesktopManager _vdm =
+            (IVirtualDesktopManager)new VirtualDesktopManager();
+
+        /// <summary>Возвращает все открытые окна всех виртуальных рабочих столов.</summary>
         public static IReadOnlyList<WindowInfo> GetAllOpenedWindows()
         {
             var list = new List<WindowInfo>();
+
+            // карта desktopGuid -> index
+            var desktopMap = BuildDesktopMap();
+
             EnumWindows((hWnd, lParam) =>
             {
                 if (!IsWindowVisible(hWnd)) return true;
 
-                // Исключаем «инструментальные» и «всплывающие» окна
                 int exStyle = (int)GetWindowLongPtr(hWnd, GWL_EXSTYLE);
                 bool isToolWindow = (exStyle & WS_EX_TOOLWINDOW) != 0;
                 bool isAppWindow = (exStyle & WS_EX_APPWINDOW) != 0;
                 if (isToolWindow && !isAppWindow) return true;
 
-                var title = new System.Text.StringBuilder(512);
+                var title = new StringBuilder(512);
                 GetWindowText(hWnd, title, title.Capacity);
 
-                if (string.IsNullOrWhiteSpace(title.ToString())) return true;
-
                 _vdm.GetWindowDesktopId(hWnd, out Guid desktopGuid);
-                int desktopIndex = GetDesktopIndex(desktopGuid);
+                desktopMap.TryGetValue(desktopGuid, out int desktopIndex);
 
                 list.Add(new WindowInfo
                 {
                     Hwnd = hWnd.ToInt64(),
-                    Title = title.ToString(),
-                    Icon = GetIcon(hWnd),
+                    Title = title.Length == 0 ? "<no title>" : title.ToString(),
+                    Icon = null, // если нужны иконки – возвращайте GetIcon(hWnd)
                     DesktopIndex = desktopIndex
                 });
                 return true;
             }, IntPtr.Zero);
-
+            
+            DebugList(list);
             return list;
         }
 
-        #region helpers
-        private static int GetDesktopIndex(Guid desktopGuid)
+        private static void DebugList(List<WindowInfo> ls)
         {
-            // Если GUID совпадает с текущим рабочим столом – вернём 0.
-            // Для остальных можно сохранять словарь desktopGuid -> index,
-            // но требуется полный IVirtualDesktopManager (более сложный код).
-            return 0; // упрощённая версия
+            foreach (WindowInfo info in ls) 
+            {
+                info.DebugInfo();
+            }
         }
 
-        private static BitmapImage GetIcon(IntPtr hWnd)
+        private static Dictionary<Guid, int> BuildDesktopMap()
         {
+            var map = new Dictionary<Guid, int>();
             try
             {
-                uint pid;
-                GetWindowThreadProcessId(hWnd, out pid);
-                var proc = System.Diagnostics.Process.GetProcessById((int)pid);
-                using var icon = Icon.ExtractAssociatedIcon(proc.MainModule.FileName);
-                if (icon == null) return null;
+                var sp = (IServiceProvider)new VirtualDesktopManager();
+                Guid iidIVDM = new Guid("F31574D6-B682-4CDC-BD56-1827860ABEC6");
+                sp.QueryService(ref iidIVDM, ref iidIVDM, out object ppv);
+                var vdm = (IVirtualDesktopManagerInternal)ppv;
 
-                using var ms = new MemoryStream();
-                icon.ToBitmap().Save(ms, ImageFormat.Png);
-                ms.Position = 0;
-
-                var bitmap = new BitmapImage();
-                // синхронно
-                bitmap.SetSource(ms.AsRandomAccessStream());
-                return bitmap;
+                vdm.GetDesktops(out IObjectArray arr);
+                arr.GetCount(out int count);
+                for (int i = 0; i < count; i++)
+                {
+                    arr.GetAt(i, typeof(IVirtualDesktop).GUID, out object deskObj);
+                    var desktop = (IVirtualDesktop)deskObj;
+                    map[desktop.GetId()] = i;
+                }
             }
-            catch
-            {
-                return null;
-            }
-            #endregion
+            catch { /* старые ОС без виртуальных рабочих столов */ }
+            return map;
         }
     }
 
@@ -131,5 +160,7 @@ namespace show_windows_button
         public string Title { get; init; } = string.Empty;
         public BitmapImage? Icon { get; init; }
         public int DesktopIndex { get; init; }
+
+        public void DebugInfo() => Debug.WriteLine($"[WindowInfo] : DebugInfo : DesktopIndex={DesktopIndex}, Hwnd={Hwnd}, Title={Title}, IconPixelHeight={Icon?.PixelHeight}");
     }
 }
